734e513c005df5f9ce3791921094020bedf97a80
Gustavo T. Bauke <baukegustavo@gmail.com>
2023-08-22 00:42:13 -0300

    adds basic player data class and rendering

--- src/Game.hs
+++ src/Game.hs
@@ -1,9 +1,9 @@
 {-# LANGUAGE ImportQualifiedPost #-}
 
-module Game (State (..), World (..), hasKey, insertKey, deleteKey, newState) where
+module Game (State (..), World (..), Player (..), hasKey, insertKey, deleteKey, newState) where
 
 import Data.Set qualified as S
-import Graphics.Gloss.Interface.IO.Interact (Key)
+import Graphics.Gloss.Interface.IO.Interact (Key, Point)
 
 data State a = State
   { sData :: a,
@@ -19,7 +19,23 @@ insertKey k s = s {sKeys = S.insert k (sKeys s)}
 deleteKey :: Key -> State a -> State a
 deleteKey k s = s {sKeys = S.delete k (sKeys s)}
 
+data Player = Player
+  { pPos :: Point
+  }
+
 data World = World
+  { wPlayer :: Player
+  }
 
 newState :: State World
-newState = State World S.empty
+newState =
+  State
+    { sData =
+        World
+          { wPlayer =
+              Player
+                { pPos = (0, 0)
+                }
+          },
+      sKeys = S.empty
+    }
--- src/Main.hs
+++ src/Main.hs
@@ -1,15 +1,21 @@
 module Main (main) where
 
 import Config (fps)
-import Game (State (State), World (World), newState)
-import Graphics.Gloss (Display (InWindow), Picture, black, circle, color, play, yellow)
-import Graphics.Gloss.Interface.IO.Game (Event)
+import Game (Player (pPos), State (State, sData), World (World, wPlayer), deleteKey, insertKey, newState)
+import Graphics.Gloss (Display (InWindow), Picture, black, circle, color, play, translate, yellow)
+import Graphics.Gloss.Interface.IO.Game (Event (EventKey), KeyState (Down, Up))
 
 render :: State World -> Picture
-render state = color yellow $ circle 80
+render state = color yellow $ translate x y $ circle 6
+  where
+    (x, y) = pPos $ wPlayer $ sData state
 
 handleEvents :: Event -> State World -> State World
-handleEvents event state = state
+handleEvents (EventKey key keyState _ _) state
+  | keyState == Down = insertKey key state
+  | keyState == Up = deleteKey key state
+  | otherwise = state
+handleEvents _ state = state
 
 update :: Float -> State World -> State World
 update dt state = state
