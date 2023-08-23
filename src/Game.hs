b069db2c866b8674dea8c9d5e7492688876d38b0
LeonardoCBoar <leonardodicredico@gmail.com>
2023-08-23 17:03:05 -0300

    Feat: Added player movement and ACTION system

--- src/Game.hs
+++ src/Game.hs
@@ -1,16 +1,19 @@
 {-# LANGUAGE ImportQualifiedPost #-}
 {-# OPTIONS_GHC -Wno-partial-fields #-}
 
-module Game (State (..), World (..), Player (..), Asset (..), AssetId (..), Map (..), Tile (..), Grid, isKeyPressed, insertKey, deleteKey, newState, tileFromAsset, findAssetId, getAssetFromId) where
+module Game (State (..), World (..), Player (..), Asset (..), AssetId (..), Map (..), Tile (..), Grid, Action(..),
+  isValidAction, updatePlayer, updateWorld, isKeyPressed, insertKey, deleteKey, newState, tileFromAsset, findAssetId, getAssetFromId) where
 
 import Data.Function (on)
 import Data.Set qualified as S
-import Graphics.Gloss.Interface.IO.Interact (Key, Picture, Point)
+import Graphics.Gloss.Interface.IO.Interact (Key, Picture, Point, Key(..), SpecialKey(..))
 import System.FilePath (splitExtension)
 
 data State a = State
   { sData :: a,
-    sKeys :: S.Set Key
+    sKeys :: S.Set Key,
+    updateTimer :: Float,
+    playerAction :: Action
   }
 
 isKeyPressed :: Key -> State a -> Bool
@@ -22,9 +25,27 @@ insertKey k s = s {sKeys = S.insert k (sKeys s)}
 deleteKey :: Key -> State a -> State a
 deleteKey k s = s {sKeys = S.delete k (sKeys s)}
 
+data Action = NoAction | Move Point | Attack Point
+isValidAction :: Action -> Bool
+isValidAction NoAction = False
+isValidAction _ = True
+
 data Player = Player
   { pPos :: Point
   }
+
+updateWorld :: State World -> World
+updateWorld state = world {wPlayer = player}
+  where
+    world = sData state
+    player = updatePlayer (playerAction state) (wPlayer world)
+
+updatePlayer :: Action -> Player -> Player
+updatePlayer (Move (x, y)) player = player {pPos = (pX + x, pY + y)}
+    where
+      pX = fst $ pPos player
+      pY = snd $ pPos player
+updatePlayer _ player = player
 
 data Asset = Asset
   { aName :: String,
@@ -57,7 +78,9 @@ newState assets map_ =
             wMap = map_,
             wAssets = S.fromList assets
           },
-      sKeys = S.empty
+      sKeys = S.empty,
+      updateTimer = 0.0,
+      playerAction = NoAction
     }
 
 findAssetId :: State World -> (Asset -> Bool) -> AssetId
--- src/Main.hs
+++ src/Main.hs
@@ -1,25 +1,50 @@
 module Main (main) where
 
 import Config (fps)
-import Game (Asset (Asset), AssetId (AssetId), Map (..), Player (pPos), State (State, sData), Tile (..), World (World, wPlayer), deleteKey, insertKey, newState)
+import Game (Asset (Asset), AssetId (AssetId), Map (..), Player (pPos), updatePlayer, State (State, sData, updateTimer, playerAction),
+  Action(..), Tile (..), World (World, wPlayer), updateWorld, deleteKey, insertKey, newState, isValidAction, updateWorld)
 import Graphics.Gloss (Display (InWindow), Picture, black, circle, color, loadBMP, play, translate, yellow)
-import Graphics.Gloss.Interface.IO.Game (Event (EventKey), KeyState (Down, Up))
+import Graphics.Gloss.Interface.IO.Game (Event (EventKey), KeyState (Down, Up), Key (SpecialKey), SpecialKey(..))
 import Renderer (renderMap)
 import System.Directory (getDirectoryContents)
 import System.FilePath (splitExtension, (</>))
 
+--REMOVER DEPOIS DE TESTAR!!!!!!!!!!!!!
+import Debug.Trace
+--REMOVER DEPOIS DE TESTAR!!!!!!!!!!!!!
+
 render :: State World -> Picture
 render = renderMap
 
+getActionFromKey :: Key -> Action
+getActionFromKey (SpecialKey KeyLeft)  = Move (-1, 0)
+getActionFromKey (SpecialKey KeyRight) = Move ( 1, 0)
+getActionFromKey (SpecialKey KeyUp)    = Move (0 ,-1)
+getActionFromKey (SpecialKey KeyDown)  = Move (0 , 1)
+getActionFromKey _ = NoAction
+
+updateInterval :: Float
+updateInterval = 0.5
+
 handleEvents :: Event -> State World -> State World
 handleEvents (EventKey key keyState _ _) state
+  | updateTimer state < updateInterval = do trace "ignored action" state
+  | keyState == Down && key `elem` actionKeys = state {playerAction = getActionFromKey key}
   | keyState == Down = insertKey key state
   | keyState == Up = deleteKey key state
   | otherwise = state
+  where 
+    actionKeys = [SpecialKey KeyUp, SpecialKey KeyDown, SpecialKey KeyLeft, SpecialKey KeyRight]
 handleEvents _ state = state
 
 update :: Float -> State World -> State World
-update dt state = state
+update dt state
+  | isValidAction action = do trace (show $ pPos $ wPlayer $ sData state) 
+                                state {sData = updateWorld state, updateTimer = 0, playerAction = NoAction}
+  | otherwise =  state{updateTimer = curUpdateTimer}
+    where 
+      action = playerAction state
+      curUpdateTimer = updateTimer state + dt
 
 mapNameToPath :: String -> String
 mapNameToPath mapName = "assets" </> "maps" </> (mapName ++ "_layer0" ++ ".bmp")
